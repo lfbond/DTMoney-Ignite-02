c342b9ede7ed0af5f79d560a1fdab81e27c44400
Luis Felipe de Souza Bond <luisfelipe.bond@gmail.com>
2023-03-27 17:27:57 -0300

    Segundo Commit-Configuração da Aplicação

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,8 @@
+import { useContext, useEffect, useState } from 'react'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 import { SearchForm } from './SearchForm'
 import {
   PriceHighLight,
@@ -8,6 +11,8 @@ import {
 } from './styles'
 
 export function Transactions() {
+  const { transactions } = useContext(TransactionsContext)
+
   return (
     <div>
       <Header />
@@ -17,95 +22,23 @@ export function Transactions() {
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de Site</td>
-              <td>
-                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Hamburger</td>
-              <td>
-                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
-              </td>
-              <td>Alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Aluguél do Apartamento</td>
-              <td>
-                <PriceHighLight variant="outcome">- R$ 1.200,00</PriceHighLight>
-              </td>
-              <td>Casa</td>
-              <td>27/03/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Computador</td>
-              <td>
-                <PriceHighLight variant="income">R$ 5.400,00</PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>15/03/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Desenvolvimento de Site</td>
-              <td>
-                <PriceHighLight variant="income">R$ 8.000,00</PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>13/03/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Janta</td>
-              <td>
-                <PriceHighLight variant="outcome">- R$ 39,00</PriceHighLight>
-              </td>
-              <td>Alimentação</td>
-              <td>13/04/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Aluguél do Apartamento</td>
-              <td>
-                <PriceHighLight variant="outcome">- R$ 1.200,00</PriceHighLight>
-              </td>
-              <td>Casa</td>
-              <td>27/02/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Sálario</td>
-              <td>
-                <PriceHighLight variant="income">R$ 5.400,00</PriceHighLight>
-              </td>
-              <td>Salário</td>
-              <td>15/02/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Almoço</td>
-              <td>
-                <PriceHighLight variant="outcome">- R$ 30,00</PriceHighLight>
-              </td>
-              <td>Alimentação</td>
-              <td>05/02/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Fone de Ouvido</td>
-              <td>
-                <PriceHighLight variant="income">- R$ 150,00</PriceHighLight>
-              </td>
-              <td>Itens</td>
-              <td>02/02/2022</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
