61cc69b7c8e004a29697459b137c614e63143123
Luis Felipe de Souza Bond <luisfelipe.bond@gmail.com>
2023-03-28 18:12:43 -0300

    terceiro commit-performace

--- src/pages/Transactions/SearchForm/index.tsx
+++ src/pages/Transactions/SearchForm/index.tsx
@@ -1,10 +1,11 @@
 import { MagnifyingGlass } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { useContextSelector } from 'use-context-selector'
 import { SearchFormContainer } from './style'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useContext } from 'react'
 import { TransactionsContext } from '../../../contexts/TransactionsContext'
+/* import { memo } from 'react' */
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -13,7 +14,12 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext)
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
 
   const {
     register,
@@ -41,3 +47,5 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+/* export const SearchForm = memo(SearchFormComponent) */
